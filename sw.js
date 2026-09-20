3c2b75afdc1f29eaf3a44f3828b5dab346f3d56a
raflicit29-hash <raflicit29@gmail.com>
2026-09-20 15:09:49 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "mrall-final-design-20260920-03-mobile";
+const SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL).catch(()=>{})));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE_NAME).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+});
